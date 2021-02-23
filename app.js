c8ad7a04cace83c44933cfbaeaa642344792c6e4
MELI CANTAMUTTO <MELICANTAMUTTO@gmail.com>
2021-02-23 20:49:09 -0300

    feat: created all modals, missing the button events

--- app.js
+++ app.js
@@ -71,3 +71,100 @@ window.addEventListener('click', e =>{
         },400)
     }
 })
+
+
+
+
+
+//SWEET ALERT
+
+const modalWelcomeModal = () =>{
+    swal({
+        title: "Bienvenidas!",
+        text: "En MatcheADAs tu objetivo es juntar tres o más ítems del mismo tipo, ya sea en fila o columna. Para eso, selecciona un ítem y a continuación un ítem adyacente para intercambiarlos de lugar.",
+        text: "Si se forma un grupo, esos ítems se eliminarán y ganarás puntos. ¡Sigue armando grupos de 3 o más antes de que se acabe el tiempo!",
+        title: "Controles",
+        text: "Click izquierdo: selección",
+        text: "Enter o Espacio: selección",
+        text: "Flechas o WASD: movimiento e intercambio",
+        button: "A jugar!",
+      });
+}
+
+const difficultyModal = () =>{
+    swal({
+        title: "Nuevo juego",
+        text: "Selecciona una dificultad",
+        buttons: {
+            easy: {
+                text: "Normal",
+                value: "easy",
+              },
+            normal: {
+              text: "Normal",
+              value: "normal",
+            },
+            hard: {
+                text: "Difícil",
+                value: "hard",
+              },
+          },
+      });
+}
+.then((value) =>{
+    switch(value){
+        case "easy":
+            createGrid(easy);
+            break;
+        case "normal":
+            createGrid(normal);
+            break;
+        case "hard":
+            createGrid(hard);
+            break;
+        default:
+    }
+})
+
+const restartModal = () =>{
+    swal({
+        title: "Reiniciar juego?",
+        text: "Perderás todo tu puntaje acumulado!",
+        buttons: {
+            cancel: {
+                text: "Cancelar",
+                value: "cancel",
+              },
+            newGame: {
+              text: "Nuevo Juego",
+              value: "newGame",
+            },
+          },
+      });
+}
+.then((value) =>{
+    switch(value){
+        case "cancel":
+            createGrid(easy);
+            break;
+        case "newGame":
+            difficultyModal();
+            break;
+        default:
+    }
+})
+const gameOverModal = () =>{
+    swal({
+        title: "¡Juego terminado!",
+        text: `Puntaje final: ${finalScore}`,
+        icon: "success",
+        button: "Nuevo Juego",
+        button: "Reiniciar",
+      });
+}
+
+
+window.addEventListener('click', ()=>{
+   modalWelcome;
+})
+
